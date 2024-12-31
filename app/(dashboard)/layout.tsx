5cd8c5b0c59a2e66efcae89006285249e8c73b79
Snippet <princeak22486@gmail.com>
2024-12-31 09:18:30 +0530

    improved the ui/ux of the application

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,19 +4,19 @@ import { getApiLimitCount } from "@/lib/api-limit";
 import { checkSubscription } from "@/lib/subscription";
 
 const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
-  const apiLimitCount = await getApiLimitCount(); //  fetching the api limit in the server component as the server component has access to prisma
+  const apiLimitCount = await getApiLimitCount();
   const isPro = await checkSubscription();
+
   return (
-    <div className="h-full relative">
-      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 bg-gray-900">
+    <div className="relative h-full bg-background-DEFAULT dark:bg-background-dark">
+      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 bg-white dark:bg-gray-900 border-r">
         <Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />
       </div>
       <main className="md:pl-72">
         <Navbar />
-        {children}
+        <div className="p-8">{children}</div>
       </main>
     </div>
   );
 };
-
 export default DashboardLayout;
